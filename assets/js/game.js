f34ac0e295815a60c3fc6340d8281c359d24722b
JoshPrice315 <61852564+JoshPrice315@users.noreply.github.com>
2020-03-23 19:05:32 -0700

    3.1.5

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,11 @@
-window.alert("This is an alert! JavaScript is running!");
+var playername = window.prompt("What is your robot's name?");
+console.log(playername);
+console.log("This logs a string, good for leaving yourself a message");
+console.log(10 + 10);
+console.log("Our robot's name is " + playername);
+
+// this creates a function named "fight"
+function fight() {
+  window.alert("The fight has begun!");
+}
+fight();
