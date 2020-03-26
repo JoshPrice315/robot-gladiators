38505d0774797cda0ec3a27e77f8cb07efdcc20d
JoshPrice315 <61852564+JoshPrice315@users.noreply.github.com>
2020-03-26 15:14:10 -0700

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,13 +3,20 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
+// function to generate a random numeric value
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+  return value;
+};
+
 // You can also log multiple values at once like this
 //console.log(playerName, playerHealth, playerAttack);
 
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
-debugger;
+
 var fight = function(enemyName) {
   while (playerHealth > 0 && enemyHealth > 0) {
     // ask user if they'd liked to fight or run
@@ -17,8 +24,10 @@ var fight = function(enemyName) {
       'Would you like FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.'
     );
     if (promptFight === "fight" || promptFight === "FIGHT") {
-      // remove enemy's health by subtracting the amount set in the playerAttack variable
-      enemyHealth = enemyHealth - playerAttack;
+      // generate random damage value based on player's attack power
+      var damage = randomNumber(playerAttack - 3, playerAttack);
+      enemyHealth = Math.max(0, enemyHealth - damage);
+
       console.log(
         playerName +
           " attacked " +
@@ -42,9 +51,9 @@ var fight = function(enemyName) {
       } else {
         window.alert(enemyName + " still has " + enemyHealth + " health left.");
       }
-
       // remove players's health by subtracting the amount set in the enemyAttack variable
-      playerHealth = playerHealth - enemyAttack;
+      var damage = randomNumber(enemyAttack - 3, enemyAttack);
+      playerHealth = Math.max(0, playerHealth - damage);
       console.log(
         enemyName +
           " attacked " +
@@ -77,7 +86,7 @@ var fight = function(enemyName) {
       if (confirmSkip) {
         window.alert(playerName + " has decided to skip this fight. Goodbye!");
         // subtract money from playerMoney for skipping
-        playerMoney = playerMoney - 10;
+        playerMoney = Math.max(0, playerMoney - 10);
         console.log("playerMoney", playerMoney);
         break;
       } else {
@@ -102,7 +111,7 @@ var startGame = function() {
       var pickedEnemyName = enemyNames[i];
 
       // reset enemyHealth before starting new fight
-      enemyHealth = 50;
+      enemyHealth = randomNumber(40, 60);
 
       // use debugger to pause script from running and check what's going on at that moment in the code
 
